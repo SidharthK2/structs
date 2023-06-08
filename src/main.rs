9adcc3a04ebdeeebb42485178bf0860469bb2135
sidharthK2 <sidharthkapoor.100@gmail.com>
2023-06-08 18:46:54 +0100

    add associated fn

--- src/main.rs
+++ src/main.rs
@@ -5,12 +5,19 @@ struct Rectangle {
 }
 
 impl Rectangle {
-    fn area(&self) -> u32 {
-        self.width * self.height
+    fn area(thing: &Rectangle) -> u32 {
+        thing.width * thing.height
     }
 
     fn can_hold(&self, rect: &Rectangle) -> bool {
         self.width > rect.width && self.height > rect.height
+    }
+
+    fn square(size: u32) -> Self {
+        Self {
+            width: size,
+            height: size,
+        }
     }
 }
 
@@ -23,10 +30,13 @@ fn main() {
         width: 10,
         height: 40,
     };
-    println!(
-        "can {:?} hold {:?}? {}.",
-        rect1,
-        rect2,
-        rect1.can_hold(&rect2)
-    )
+    let sq = Rectangle::square(10);
+    println!("{:?}", sq);
+    // println!(
+    //     "can {:?} hold {:?}? {}.",
+    //     rect1,
+    //     rect2,
+    //     rect1.can_hold(&rect2)
+    // )
+    // println!("area is {}", Rectangle::area(&rect1))
 }
